8061910c931f09bdb83b631c5795f501ba8d3d5c
Y0nxy <63762687+Y0nxy@users.noreply.github.com>
2025-09-28 03:06:10 +0300

    Update friendACBottom.js
    it broke, so i fixed it

--- friendACBottom.js
+++ friendACBottom.js
@@ -3,26 +3,24 @@
 // DESCRIPTION: Moves Friend Activity Button Down.
 
 (async function friendACBottom() {
-	if (!Spicetify.CosmosAsync) {
-		setTimeout(friendACBottom, 10);
+	if (!(Spicetify.CosmosAsync && Spicetify.URI)) {
+		setTimeout(friendACBottom, 300);
 		return;
 	}
 	
-
+	//console.log("Started")
 	// Select the button using its aria-label attribute
-	//var button = document.querySelector('.encore-text-body-small-bold main-topBar-buddyFeed[aria-label="Friend Activity"]');
-	var button = document.querySelector('button[aria-label="Friend Activity"]');
-	//console.log("TEXT");
+	var button = document.querySelector('[aria-label="Friend Activity"]');
+
 	// Check if the button is found
 	if (button) {
-
 		// Select the target container using its class
-		//console.log("found button");
 		var targetContainer = document.querySelector('.main-nowPlayingBar-extraControls');
+		//console.log("Found!")
+
 		button.style.background = 'transparent';
 		// Append the button to the target container
 		if (targetContainer) {
-			//targetContainer.insertBefore(button.parentElement, targetContainer.firstChild);
 			targetContainer.insertBefore(button, targetContainer.firstChild);
 		}
 	}
